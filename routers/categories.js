2639565634bc99503799cbe557bda6928f7468bc
rn-lazuka <rn-lazuka@github.com>
2022-07-01 13:29:29 +0300

    added categories and product requests

--- routers/categories.js
+++ routers/categories.js
@@ -7,7 +7,62 @@ router.get('/', async (req, res) => {
     if (!categoryList) {
         res.status(500).json({ success: false })
     }
-    res.send(categoryList)
+    res.status(200).send(categoryList)
 })
 
-module.exports = router;
+router.get('/:id', async (req, res) => {
+    const category = await Category.findById(req.params.id)
+    if (!category) {
+        res.status(500).json({
+            message: 'The category with given ID was not found!',
+        })
+    }
+    res.status(200).send(category)
+})
+
+router.post('/', async (req, res) => {
+    let category = new Category({
+        name: req.body.name,
+        icon: req.body.icon,
+        color: req.body.color,
+    })
+
+    category = await category.save()
+
+    !category
+        ? res.status(404).send('the category cannot be created!')
+        : res.send(category)
+})
+
+router.put('/:id', async (req, res) => {
+    let category = await Category.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            icon: req.body.icon,
+            color: req.body.color,
+        },
+        { new: true }
+    )
+
+    !category
+        ? res.status(404).send('the category cannot be created!')
+        : res.send(category)
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const category = await Category.findByIdAndDelete(req.params.id)
+        !category
+            ? res
+                  .status(404)
+                  .json({ status: false, message: 'category not found!' })
+            : res
+                  .status(200)
+                  .json({ status: true, message: 'the category is deleted!' })
+    } catch (error) {
+        res.status(400).json({ status: false, error })
+    }
+})
+
+module.exports = router
--- routers/products.js
+++ routers/products.js
@@ -1,33 +1,60 @@
 const express = require('express')
 const router = express.Router()
 const { Product } = require('../models/product')
-
+const { Category } = require('../models/category')
 
 router.get('/', async (req, res) => {
-    const productList = await Product.find()
+    const productList = await Product.find().select('name image -_id').populate('category')
     if (!productList) {
         res.status(500).json({ success: false })
     }
     res.send(productList)
 })
 
-router.post('/', (req, res) => {
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        stock: req.body.stock,
-    })
-    product
-        .save()
-        .then((createdProduct) => {
-            res.status(201).json(createdProduct)
-        })
-        .catch((err) => {
-            res.status(500).json({
-                error: err,
-                success: false,
-            })
-        })
+router.get('/:id', async (req, res) => {
+    const product = await Product.findById(req.params.id).populate('category')
+    if (!product) {
+        res.status(500).json({ success: false, message: 'The product with given ID was not found!' })
+    }
+    res.status(200).send(product)
 })
 
-module.exports = router;
+router.post('/', async (req, res) => {
+    const category = await Category.findById(req.body.category)
+    if (!category) res.status(400).send('invalid category!')
+
+    let product = new Product({
+        name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
+        image: req.body.image,
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured,
+    })
+    product = await product.save()
+    !product
+        ? res.status(404).send('the product cannot be created!')
+        : res.send(product)
+})
+
+router.delete('/:id', async (req,res)=>{
+    try {
+        const product = await Product.findByIdAndRemove(req.params.id)
+        !product
+            ? res
+                .status(404)
+                .json({ status: false, message: 'product not found!' })
+            : res
+                .status(200)
+                .json({ status: true, message: 'the product is deleted!' })
+    } catch (error) {
+        res.status(400).json({ status: false, error })
+    }
+})
+
+module.exports = router
