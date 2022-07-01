114bfd195d4e925987c612ce48fdf1348cf862a9
rn-lazuka <rn-lazuka@github.com>
2022-07-01 16:36:30 +0300

    added user schema

--- models/user.js
+++ models/user.js
@@ -1,11 +1,45 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    stock: {
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    passwordHash: {
+        type: String,
+        required: true,
+    },
+    street: {
+        type: String,
+        default: '',
+    },
+    apartment: {
+        type: String,
+        default: '',
+    },
+    city: {
+        type: String,
+        default: '',
+    },
+    zip: {
+        type: String,
+        default: '',
+    },
+    country: {
+        type: String,
+        default: '',
+    },
+    phone: {
         type: Number,
         required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
     },
 })
 
@@ -16,3 +50,4 @@ userSchema.virtual('id').get(function () {
 userSchema.set('toJSON', { virtuals: true })
 
 exports.User = mongoose.model('User', userSchema)
+exports.userSchema = userSchema
