91a6f47f4abd078799d48f29e5d12b4dd4c0b38f
rn-lazuka <rn.lazuka@gmail.com>
2022-07-04 13:06:48 +0300

    added creating user and hashing password

--- routers/users.js
+++ routers/users.js
@@ -1,20 +1,32 @@
 const express = require('express')
 const router = express.Router()
-const { User } = require('../models/user')
+const {User} = require('../models/user')
+const bcrypt = require('bcryptjs')
+const {Category} = require("../models/category");
 
 router.get('/', async (req, res) => {
-    const productList = await User.find()
+    const productList = await User.find().select('-passwordHash')
     if (!productList) {
-        res.status(500).json({ success: false })
+        res.status(500).json({success: false})
     }
     res.send(productList)
+})
+
+router.get('/:id', async (req, res) => {
+    const user = await User.findById(req.params.id).select('-passwordHash')
+    if (!user) {
+        res.status(500).json({
+            message: 'The user with given ID was not found!',
+        })
+    }
+    res.status(200).send(user)
 })
 
 router.post('/', async (req, res) => {
     let user = new User({
         name: req.body.name,
         email: req.body.email,
-        passwordHash: req.body.passwordHash,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
         street: req.body.street,
         apartment: req.body.apartment,
         city: req.body.city,
