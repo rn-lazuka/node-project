41da1e723ec2b29cc5c99edd7c34455fb729202f
Roman Lazuka <rn.lazuka@gmail.com>
2022-07-07 15:42:23 +0300

    added order schema and route for creating order

--- routers/orders.js
+++ routers/orders.js
@@ -1,13 +1,47 @@
 const express = require('express')
 const router = express.Router()
-const { Order } = require('../models/order')
+const {Order} = require('../models/order')
+const {OrderItem} = require('../models/order-item')
 
 router.get('/', async (req, res) => {
     const productList = await Order.find()
     if (!productList) {
-        res.status(500).json({ success: false })
+        res.status(500).json({success: false})
     }
     res.send(productList)
 })
 
-module.exports = router;
+router.post('/', async (req, res) => {
+    const orderItemsIds = Promise.all(
+        req.body.orderItems.map(async (orderItem) => {
+            let newOrderItem = new OrderItem({
+                quantity: orderItem.quantity,
+                product: orderItem.product,
+            })
+            newOrderItem = await newOrderItem.save()
+            return newOrderItem.id
+        })
+    )
+
+    const orderItemsIdsResolved = await orderItemsIds
+
+    let order = new Order({
+        orderItems: orderItemsIdsResolved,
+        shippingAddress1: req.body.shippingAddress1,
+        shippingAddress2: req.body.shippingAddress2,
+        city: req.body.city,
+        zip: req.body.zip,
+        country: req.body.country,
+        phone: req.body.phone,
+        status: req.body.status,
+        totalPrice: req.body.totalPrice,
+        user: req.body.user,
+        dateOrder: req.body.dateOrder,
+    })
+    order = await order.save()
+    !order
+        ? res.status(404).send('the order cannot be created!')
+        : res.send(order)
+})
+
+module.exports = router
