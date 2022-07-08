7dd04c653053c6d116108c9d4d0eaeb66a844a38
Roman Lazuka <rn.lazuka@gmail.com>
2022-07-08 15:45:12 +0300

    feat: finished orders routes

--- routers/orders.js
+++ routers/orders.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const { Order } = require('../models/order')
 const { OrderItem } = require('../models/order-item')
+const { Category } = require('../models/category')
+const { User } = require('../models/user')
 
 router.get('/', async (req, res) => {
     const orderList = await Order.find()
@@ -19,7 +21,8 @@ router.get('/:id', async (req, res) => {
         .populate({
             path: 'orderItems',
             populate: {
-                path: 'product', populate: 'category'
+                path: 'product',
+                populate: 'category',
             },
         })
 
@@ -43,6 +46,18 @@ router.post('/', async (req, res) => {
 
     const orderItemsIdsResolved = await orderItemsIds
 
+    const totalPrices = await Promise.all(
+        orderItemsIdsResolved.map(async (orderItemId) => {
+            const orderItem = await OrderItem.findById(orderItemId).populate(
+                'product',
+                'price'
+            )
+            return orderItem.product.price * orderItem.quantity
+        })
+    )
+
+    const totalPrice = totalPrices.reduce((a, b) => a + b, 0)
+
     let order = new Order({
         orderItems: orderItemsIdsResolved,
         shippingAddress1: req.body.shippingAddress1,
@@ -52,7 +67,7 @@ router.post('/', async (req, res) => {
         country: req.body.country,
         phone: req.body.phone,
         status: req.body.status,
-        totalPrice: req.body.totalPrice,
+        totalPrice,
         user: req.body.user,
         dateOrder: req.body.dateOrder,
     })
@@ -60,6 +75,78 @@ router.post('/', async (req, res) => {
     !order
         ? res.status(404).send('the order cannot be created!')
         : res.send(order)
+})
+
+router.put('/:id', async (req, res) => {
+    let order = await Order.findByIdAndUpdate(
+        req.params.id,
+        {
+            status: req.body.status,
+        },
+        { new: true }
+    )
+
+    !order
+        ? res.status(404).send('Can not find order with such ID!')
+        : res.send(order)
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const order = await Order.findByIdAndDelete(req.params.id)
+        if (order) {
+            await order.orderItems.map(async (orderItem) => {
+                await OrderItem.findByIdAndDelete(orderItem._id)
+            })
+            return res
+                .status(200)
+                .json({ status: true, message: 'the order is deleted!' })
+        }
+        return res
+            .status(404)
+            .json({ status: false, message: 'order not found!' })
+    } catch (error) {
+        res.status(400).json({ status: false, error })
+    }
+})
+
+router.get(`/get/totalsales`, async (req, res) => {
+    const totalSales = await Order.aggregate([
+        {
+            $group: { _id: null, totalSales: { $sum: '$totalPrice' } },
+        },
+    ])
+    if (!totalSales) {
+        res.status(400).json({
+            success: false,
+            message: 'The order sales can not be generated!',
+        })
+    }
+    res.status(200).send({ totalSales: totalSales.pop().totalSales })
+})
+
+router.get(`/get/count`, async (req, res) => {
+    const orderCount = await Order.countDocuments()
+    if (!orderCount) {
+        res.status(500).json({ success: false, message: 'no orders' })
+    }
+    res.status(200).json({ total: orderCount })
+})
+
+router.get(`/get/userorders/:userid`, async (req, res) => {
+    const userOrderList = await Order.find({
+        user: req.params.userid,
+    }).populate({
+        path: 'orderItems',
+        populate: {
+            path: 'product',
+            populate: 'category',
+        }
+    }).sort({ dateOrder: -1 })
+    if (!userOrderList) {
+        res.status(500).json({ success: false, message: 'no orders' })
+    }
+    res.status(200).send(userOrderList)
 })
 
 module.exports = router
